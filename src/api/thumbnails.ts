8d670e22bb4fd38df902efd5cf9b1403b568b068
Philipp Spitzley <129886018+philippspitzley@users.noreply.github.com.>
2025-12-05 10:57:24 +0100

    store base64 string instead of url string in db

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,40 +1,11 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError } from "./errors";
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+const MAX_UPLOAD_SIZE = 10 << 20; // bit-shifting is same as 10 * 1024 * 1024 = 10MB
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -48,6 +19,29 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
   // TODO: implement the upload here
+  const formData = await req.formData();
+  const thumbnail = formData.get("thumbnail");
+  if (!(thumbnail instanceof File)) {
+    throw new BadRequestError("Thumbnail file is missing.");
+  }
 
-  return respondWithJSON(200, null);
+  if (thumbnail.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Thumbnail size is too big.");
+  }
+  const mediaType = thumbnail.type;
+  const imgData = await thumbnail.arrayBuffer();
+  const metaData = getVideo(cfg.db, videoId);
+
+  if (!metaData) {
+    throw new NotFoundError("Video metadata not found");
+  }
+
+  const thumbnailBuffer = Buffer.from(imgData).toString("base64");
+  const thumbnailURL = `data:${mediaType};base64,${thumbnailBuffer}`;
+
+  metaData.thumbnailURL = thumbnailURL;
+
+  updateVideo(cfg.db, metaData);
+
+  return respondWithJSON(200, metaData);
 }
